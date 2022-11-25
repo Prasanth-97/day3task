df080ec370ae6492592c93aeebcb743a6942e262
Prasanth-97 <115787721+Prasanth-97@users.noreply.github.com>
2022-11-25 20:57:06 +0530

    Add files via upload

---
+++ jsoniteration.js
@@ -0,0 +1,68 @@
+//json iteration using forin loop
+
+const myinfo = {
+    "firstname": "prasanth",
+    "lastname": "p",
+    "dob": "13/06/1997",
+    "nationality": "Indian",
+    "religion": "Hindu",
+}
+
+
+for (const key in myinfo) {
+      {
+      console.log(`:${key}: ${myinfo[key]}`);
+     }
+  };
+
+//json iteration using for loop 
+
+const myinfo1 = {
+    "firstname" : "John",
+    "lastname" : "Cena",
+    "dob": "23/04/1977",
+    "nationality": "American",
+    "religion": "christian",
+}
+let array = Object.keys(myinfo1);
+
+for (let i = 0 ; i< array.length ; i++)
+{
+    console.log(`${array[i]} : ${myinfo1[array[i]]} `);
+};
+
+//json iteration using forof loop
+
+
+const myinfo2 = {
+    "firstname" : "virat",
+    "lastname" : "kholi",
+    "dob": "05/11/1988",
+    "nationality": "Indian",
+    "religion": "Hindu",
+}
+let array1 = Object.keys(myinfo2);
+
+for (value of array1)
+{
+    console.log(`${value} : ${myinfo2[value]}`);
+}
+
+//json iteration using foreach
+
+const myinfo3 = {
+    "firstname": "Lionel",
+    "lastname": "Messi",
+    "dob": "05/02/1985",
+    "nationality": "Argentine",
+    "religion": "christian catholic",
+}
+let array2 = Object.keys(myinfo3);
+
+array2.forEach( (value) => {
+    console.log( value + ":" + myinfo3[value])
+});
+
+
+
+
---
+++ jsonresume.js
@@ -0,0 +1,43 @@
+//my resume in json format
+
+const myresume = [
+    {
+        "firstname": "prasanth",
+        "lastname": "p",
+        "dob": "13/06/1997",
+        "nationality": "Indian",
+        "religion": "Hindu",
+        "address": {
+            "doorno": "1/11",
+            "streetname": "ganesh colony",
+            "place": "Dharmapuri",
+            "pincode": 636704
+        },
+        
+        "hobby": ["programming", "playing cricket", "travelling"],
+        "phoneno": 9629448101,
+        "mail": "prasnthdpj@gmail.com",
+        "maritalstatus": "unmarried"
+    },
+    {
+        "college": "college of engineering Guindy",
+        "Qualification": "B.E",
+        "Department": "Electronics and communication Engineering",
+        "yearofgraduation": 2019
+    },
+    {
+        "schoolname": "sri vijay vidyalaya school",
+        "tenthmarkpercent": "97%",
+        "yearofpassing": 2015,
+        "twelthmarkpercent": "96.9%",
+        "yearofpassing": 2017
+    },
+    {
+        "extracurricular": ["volunteer in kurukshetra", "nss camp organizer", "TIME exam certifird member"],
+        "mygoods": ["easily understandable", "polite", "do works properly on time", "Honest"]
+    },
+    {
+        "projects": ["vehicle parking sensor", "find car parking slot in malls and booking the slot "]
+    }
+];
+console.log(myresume);
